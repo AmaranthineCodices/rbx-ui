799620e008843755fca44b9d1342b3a9d7912ece
AmaranthineCodices <AmaranthineCodices@gmail.com>
2017-12-24 16:49:53 -0500

    Spaces -> tabs

--- examples/Bars.client.lua
+++ examples/Bars.client.lua
@@ -4,16 +4,16 @@ local Bars = require(game.ReplicatedStorage.UI.spinners.Bars)
 local playerGui = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 local demo = Roact.createElement("ScreenGui", {}, {
-    Roact.createElement("Frame", {
-        Size = UDim2.new(0, 200, 0, 100),
-        Position = UDim2.new(0.5, -100, 0.5, -50),
-        BackgroundColor3 = Color3.new(1, 1, 1)
-    }, {
-        Roact.createElement(Bars, {
-            Position = UDim2.new(0.5, 0, 0.5, 0),
-            AnchorPoint = Vector2.new(0.5, 0.5)
-        })
-    })
+	Roact.createElement("Frame", {
+		Size = UDim2.new(0, 200, 0, 100),
+		Position = UDim2.new(0.5, -100, 0.5, -50),
+		BackgroundColor3 = Color3.new(1, 1, 1)
+	}, {
+		Roact.createElement(Bars, {
+			Position = UDim2.new(0.5, 0, 0.5, 0),
+			AnchorPoint = Vector2.new(0.5, 0.5)
+		})
+	})
 })
 
 Roact.reify(demo, playerGui, "BarsDemo")
